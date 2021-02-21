ad2c2ea4a68fa002f22e1ae46c275ef2cd5b7488
akubara <akubaradev@gmail.com>
2021-02-21 23:56:04 +0300

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+## Description
+Simple boilerplate, includes:  
+nodemon + express + babel + body-parser + cors + eslint + prettier
+
+## Install and run
+
+``` bash
+git clone https://github.com/akubara/lazy-express-starter.git
+npm install
+npm run 
+# ??????? 
+# PROFIT
+
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log('Hello World!'));
